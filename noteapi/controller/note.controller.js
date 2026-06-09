502efd84c015795681db395e033876b3ad49cad8
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2026-06-10 00:10:56 +0545

    creating controller and router fo the project noteapi

--- noteapi/controller/note.controller.js
+++ noteapi/controller/note.controller.js
@@ -1,3 +1,30 @@
-const getAllnodes=(req,res)=>{
-    
+const { error } = require('console')
+const fs= require('fs')
+const path=require('path')
+
+const NOTE_DIR=path.join(__dirname,'../notes')
+
+const getAllNoTes=(req,res)=>{
+
+const {search}= req.query 
+
+fs.readdir(NOTE_DIR,(err,files)=>{
+    if(err) return res
+    .status(500)
+    .json({error: "failed to read note directry"});
+
+    let notes =files.filter(file=>file.endsWith('.txt'))
+    if(search){
+        notes=notes.filter(note=> note.includes(search))
+    }
+
+     res.json({
+        success:"true",
+        data:notes,
+        total:notes.length
+     })
+})
+}
+export {
+    getAllNoTes
 }
--- noteapi/routes/note.route.js
+++ noteapi/routes/note.route.js
@@ -1,8 +1,8 @@
 const express=require('express')
 const noteRoute= express.Router()
-
+const {getAllNotes} = require("../controller/note.controller")
 // features
-noteRoute.get("/")
+noteRoute.get("/".getAllNotes)
 noteRoute.post("/")
 noteRoute.put("/")
 noteRoute.delete("/:id")
